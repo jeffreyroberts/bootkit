0cf55adaab14460222eb38b422b018abc11cd95f
root <root@ip-10-235-38-214.us-west-2.compute.internal>
2014-09-10 04:24:36 +0000

    Adding tag instance

--- boot.sh
+++ boot.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+export AWS_ACCESS_KEY='AKIAJP5YBUABRN4XK7MA'
+export AWS_SECRET_KEY='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'
+
+export JAVA_HOME=/usr
+export EC2_HOME=/opt/aws
+export PATH=$PATH:$EC2_HOME/bin
+
 if [ -f /root/boot/booted ]; then
 	mount -a >> /root/boot/logs/mount.log 2>&1
 	sleep 10
@@ -10,6 +17,14 @@ fi
 
 if [ ! -f /root/boot/booted ]; then
 	sleep 10
+
+	tag=`cat /root/boot/config/tags`
+	
+	if [[ "$tag" != "" ]]; then
+		instance=`/root/boot/bin/ec2-metadata -i | awk '{ print $2 }'`
+		ec2-create-tags $instance --tag "tag=$tag" --region us-west-2
+	fi
+
 #	/root/boot/bin/mkswap.sh >> /root/boot/logs/install.log 2>&1
 	/root/boot/bin/mnt_all.sh >> /root/boot/logs/install.log 2>&1
 	/root/boot/bin/attach_eip.sh >> /root/boot/logs/install.log 2>&1
