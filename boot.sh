49adfa53f3513811241fc729176680495a6033d4
Jeffrey L. Roberts <Jeffrey.L.Roberts@gmail.com>
2014-09-10 16:56:32 -0700

    added param to boot.sh for high availability

--- boot.sh
+++ boot.sh
@@ -29,7 +29,7 @@ if [ ! -f /root/boot/booted ]; then
 	/root/boot/bin/mnt_all.sh >> /root/boot/logs/install.log 2>&1
 	/root/boot/bin/attach_eip.sh >> /root/boot/logs/install.log 2>&1
 	/root/boot/provision.sh provision >> /root/boot/logs/provisioning.log 2>&1
-	/root/boot/bin/ha >> /root/boot/logs/ha.log 2>&1
+	/root/boot/bin/ha boot >> /root/boot/logs/ha.log 2>&1
 	touch /root/boot/booted
 	sleep 10
 	/sbin/shutdown -r now
