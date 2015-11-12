45cde474cb652d128aedb397defdc936401dd26a
Jeffrey L. Roberts <jeffrey.l.roberts@gmail.com>
2015-11-12 14:10:18 -0500

    Update myip.sh

--- bin/myip.sh
+++ bin/myip.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-export AWS_ACCESS_KEY='AKIAJP5YBUABRN4XK7MA'
-export AWS_SECRET_KEY='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'
+export AWS_ACCESS_KEY=''
+export AWS_SECRET_KEY=''
 
 export JAVA_HOME=/usr
 export EC2_HOME=/opt/aws
